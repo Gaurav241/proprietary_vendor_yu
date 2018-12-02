60a6e8af3c96e5e035b0c9e3acc153676ad25f4f
dianlujitao <dianlujitao@lineageos.org>
2018-12-02 17:04:33 +0800

    lettuce: Kang ims.apk and QTI telephony jar from daisy
     * From: daisy-user 9 PKQ1.180917.001 V10.0.1.0.PDLMIFJ release-keys
    
    Change-Id: I2f5b8e0639bf43ada956c71dafcc015c197ed28e

--- lettuce/Android.mk
+++ lettuce/Android.mk
@@ -197,22 +197,12 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := ims
 LOCAL_MODULE_OWNER := yu
-LOCAL_SRC_FILES := proprietary/vendor/app/ims/ims.apk
+LOCAL_SRC_FILES := proprietary/priv-app/ims/ims.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := imssettings
-LOCAL_MODULE_OWNER := yu
-LOCAL_SRC_FILES := proprietary/vendor/app/imssettings/imssettings.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
